95893caf61076c910e6faec64ab56d871f2ef503
MakotoFujino <116146224+MakotoFujino@users.noreply.github.com>
2022-10-27 09:37:43 +0800

    Update testtttt.py

--- testing/testtttt.py
+++ testing/testtttt.py
@@ -1,9 +1,5 @@
 from tkinter import *
 <<<<<<< HEAD
-dasadasdasda
-print('who asked')
-=======
-<<<<<<< HEAD
 #dasadasdasda
 =======
 dasadasdasda
@@ -13,4 +9,6 @@ My name is celine
 i am a good girl 
 hahahahhahah
 >>>>>>> d91bc9fd14099a2d7a147d0b1a0839737cb2d3b8
->>>>>>> b10d255efdd1954edf0e3a23e6f7751e8ad02911
+
+
+hi there this is nic poopoo
