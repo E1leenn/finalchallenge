f2ee0979125c15fbbb44e2e11d43cdde9fdafc50
celinewaiii28 <115997806+celinewaiii28@users.noreply.github.com>
2022-11-03 12:54:32 +0800

    Update test 32x32 gui.py

--- testing/test 32x32 gui.py
+++ testing/test 32x32 gui.py
@@ -1,49 +1,83 @@
-from cgi import print_arguments
 from tkinter import *
 
 def whitebtn(i, j):
-  global colour   
-  if colour == 0:
-    button[i][j].config(bg='white')
-  elif colour == 1: 
-    button[i][j].config(bg='#f3f3f3')
-  elif colour == 2:
-    button[i][j].config(bg='#dedede')
-  elif colour == 3: 
-    button[i][j].config(bg='#cccccc')
-  elif colour == 4:
-    button[i][j].config(bg='#999999')  
-  elif colour == 5: 
-    button[i][j].config(bg='#666666')
-  elif colour == 6:
-    button[i][j].config(bg='#222222')
-  else: 
-    button[i][j].config(bg='black')
+	global colour, value 
+	if colour == 0:
+		button[i][j].config(bg='grey99')
+		value[i][j] = 0
+	elif colour == 1: 
+		button[i][j].config(bg='grey88')
+		value[i][j] = 20
+	elif colour == 2:
+		button[i][j].config(bg='grey77')
+		value[i][j] = 30
+	elif colour == 3: 
+		button[i][j].config(bg='grey66')
+		value[i][j] = 40
+	elif colour == 4:
+		button[i][j].config(bg='grey44')	
+		value[i][j] = 50
+	elif colour == 5: 
+		button[i][j].config(bg='grey33')
+		value[i][j] = 60
+	elif colour == 6:
+		button[i][j].config(bg='grey11')
+		value[i][j] = 70
+	else: 
+		button[i][j].config(bg='grey11')
+		value[i][j] = 90
 
 def change_colour(m): 
-  global colour
-  colour=m 
+	global colour
+	colour=m 
 
-  print("colour is {}".format(colour))
+	print("colour is {}".format(colour))
 
 def allwht():
-  for j in range (32):
-    for i in range (32):
-      button[i][j].config(bg='white')
+	for j in range (32):
+		for i in range (32):
+			button[i][j].config(bg='grey99')
+			value[i][j] = 0
 
 def allblk():
-  for j in range (32):
-    for i in range (32):
-      button[i][j].config(bg='black')
+	for j in range (32):
+		for i in range (32):
+			button[i][j].config(bg='grey1')
+			value[i][j] = 90 
 
 def pattern():
-  for j in range (32):
-    for i in range (32):
-      if i == j: 
-        button[i][j].config(bg='grey')
-      elif i + j == 2: 
-        button[i][j].config(bg='grey')
+	for j in range (32):
+		for i in range (32):
+			if i == j: 
+				button[i][j].config(bg='grey66')
+				value[i][j] = 40
+			elif i + j == 31: 
+				button[i][j].config(bg='grey66')
+				value[i][j] = 40 
 
+def ramseq():
+	for j in range (32):
+		for i in range (32):
+			if j == 0: 
+				button[i][j].config(bg='grey66')
+				value[i][j] = 40
+			elif j == 1: 
+				button[i][j].config(bg='grey33')
+				value[i][j] = 60
+			elif j == 2: 
+				button[i][j].config(bg='grey1')
+				value[i][j] = 90   
+      elif j == 10: 
+				button[i][j].config(bg='grey33')
+				value[i][j] = 60
+			elif j == 11: 
+				button[i][j].config(bg='grey1')
+				value[i][j] = 90   
+
+
+
+def sendbtn():
+	print(value)
 
 main = Tk()
 
@@ -64,49 +98,52 @@ frame4.grid(row=2, columnspan=2) #send btn
 
 # 3x3 grid
 button = [[j for j in range(32)] for i in range(32)]
+ 
+# to display value
+value = [[0 for i in range (32)] for j in range(32)]
 
 for j in range (32):
-  for i in range (32):
-    button[i][j] = Button(frame1, width=1,height=1, command=lambda r=i, c=j:whitebtn(r, c))
-    button[i][j].grid(row=i, column=j)
+	for i in range (32):
+		button[i][j] = Button(frame1, font=("Calibri, 5"), width=1, height=1, command=lambda r=i, c=j:whitebtn(r, c))
+		button[i][j].grid(row=i, column=j)
 
-    print(button)
+		#print(button)
 
 # #shades button
-white = Button(frame2, text="White", font=("Calibri, 5"), command=lambda m=0:change_colour(m))
+white = Button(frame2, text="White", font=("Calibri, 10"), bg='grey99', width=13, height=2, command=lambda m=0:change_colour(m))
 white.grid(row=0, column=0)
-grey1 = Button(frame2, text="Grey1", font=("Calibri, 5"), bg='#f3f3f3', command=lambda m=1:change_colour(m))
+grey1 = Button(frame2, text="Grey1", font=("Calibri, 10"), bg='grey88', width=13, height=2, command=lambda m=1:change_colour(m))
 grey1.grid(row=1, column=0)
-grey2 = Button(frame2, text="Grey2", font=("Calibri, 5"), bg='#dedede', command=lambda m=2:change_colour(m))
+grey2 = Button(frame2, text="Grey2", font=("Calibri, 10"), bg='grey77', width=13, height=2, command=lambda m=2:change_colour(m))
 grey2.grid(row=2, column=0)
-grey3 = Button(frame2, text="Grey3", font=("Calibri, 5"), bg='#cccccc', command=lambda m=3:change_colour(m))
+grey3 = Button(frame2, text="Grey3", font=("Calibri, 10"), bg='grey66', width=13, height=2, command=lambda m=3:change_colour(m))
 grey3.grid(row=3, column=0)
-grey4 = Button(frame2, text="Grey4", font=("Calibri, 5"), bg='#999999', command=lambda m=4:change_colour(m))
+grey4 = Button(frame2, text="Grey4", font=("Calibri, 10"), bg='grey44', width=13, height=2, command=lambda m=4:change_colour(m))
 grey4.grid(row=4, column=0)
-grey5 = Button(frame2, text="Grey5", font=("Calibri, 5"), bg='#666666', fg='white', command=lambda m=5:change_colour(m))
+grey5 = Button(frame2, text="Grey5", font=("Calibri, 10"), bg='grey33', fg='white', width=13, height=2, command=lambda m=5:change_colour(m))
 grey5.grid(row=5, column=0)
-grey6 = Button(frame2, text="Grey6", font=("Calibri, 5"), bg='#222222', fg='white', command=lambda m=6:change_colour(m))
+grey6 = Button(frame2, text="Grey6", font=("Calibri, 10"), bg='grey11', fg='white', width=13, height=2, command=lambda m=6:change_colour(m))
 grey6.grid(row=6, column=0)
-black = Button(frame2, text="Black", font=("Calibri, 5"), bg='black', fg='white', command=lambda m=7:change_colour(m))
+black = Button(frame2, text="Black", font=("Calibri, 10"), bg='grey1', fg='white', width=13, height=2, command=lambda m=7:change_colour(m))
 black.grid(row=7, column=0)
 
 # # #colour button
-allwhite = Button(frame3, text="All White",font=("Calibri, 5"), bg='white', command=allwht)
+allwhite = Button(frame3, text="All White",font=("Calibri, 10"), bg='white', width=13, height=2, command=allwht)
 allwhite.grid(row=0, column=0)
 
-allblack = Button(frame3, text="All Black",font=("Calibri, 5"), bg='black', fg='white', command=allblk)
+allblack = Button(frame3, text="All Black",font=("Calibri, 10"), bg='black', fg='white', width=13, height=2, command=allblk)
 allblack.grid(row=0, column=1)
 
-xpattern = Button(frame3, text="X Pattern",font=("Calibri, 5"), bg='gold', command=pattern)
+xpattern = Button(frame3, text="X Pattern",font=("Calibri, 10"), bg='gold', width=13, height=2, command=pattern)
 xpattern.grid(row=0, column=2)
 
-seq = Button(frame3, text="Sequence",font=("Calibri, 5"), bg='#ff007f')
+seq = Button(frame3, text="Sequence",font=("Calibri, 10"), bg='#ff007f', width=13, height=2, command=ramseq)
 seq.grid(row=0, column=3)
 
 #send btn
-send = Button(frame4, text="Send Imaged!", font=("Calibri, 5"))
+send = Button(frame4, text="Send Imaged!", font=("Calibri, 10"), width=13, height=2, command=sendbtn)
 send.grid(row=0, column=0)
 
-print("Button is {}".format(button))
+#print("Button is {}".format(button))
 
 main.mainloop()
