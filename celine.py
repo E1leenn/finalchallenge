97b7e8ceb98eb5d91b8c675a2a1407bbe477ab24
celinewaiii28 <115997806+celinewaiii28@users.noreply.github.com>
2022-11-16 14:50:06 +0800

    Update celine.py

--- celine.py
+++ celine.py
@@ -118,6 +118,9 @@ def paint(event):
         x2, y2 = (event.x+1), (event.y+1)
         c.create_oval(x1,y1,x2,y2,fill='grey1',outline='grey11') 
 
+def clearbtn():
+    c.delete('all')
+
 main = Tk()
 main.title("Group C")
 
@@ -163,23 +166,27 @@ for j in range (32):
     button[i][j] = Button(frame1, font=("Calibri, 5"), width=1, height=1, bg='white', command=lambda r=i, c=j:whitebtn(r, c))
     button[i][j].grid(row=i, column=j)
 
+#clear button
+clear = Button(frame2, text="Clear", font=("Calibri, 10"), bg='grey99', width=13, height=2, command=clearbtn)
+clear.grid(row=0, column=0)
+
 #shades button
 white = Button(frame2, text="White", font=("Calibri, 10"), bg='grey99', width=13, height=2, command=lambda m=0:change_colour(m))
-white.grid(row=0, column=0)
+white.grid(row=1, column=0)
 grey1 = Button(frame2, text="Grey1", font=("Calibri, 10"), bg='grey88', width=13, height=2, command=lambda m=1:change_colour(m))
-grey1.grid(row=1, column=0)
+grey1.grid(row=2, column=0)
 grey2 = Button(frame2, text="Grey2", font=("Calibri, 10"), bg='grey77', width=13, height=2, command=lambda m=2:change_colour(m))
-grey2.grid(row=2, column=0)
+grey2.grid(row=3, column=0)
 grey3 = Button(frame2, text="Grey3", font=("Calibri, 10"), bg='grey66', width=13, height=2, command=lambda m=3:change_colour(m))
-grey3.grid(row=3, column=0)
+grey3.grid(row=4, column=0)
 grey4 = Button(frame2, text="Grey4", font=("Calibri, 10"), bg='grey44', width=13, height=2, command=lambda m=4:change_colour(m))
-grey4.grid(row=4, column=0)
+grey4.grid(row=5, column=0)
 grey5 = Button(frame2, text="Grey5", font=("Calibri, 10"), bg='grey33', fg='white', width=13, height=2, command=lambda m=5:change_colour(m))
-grey5.grid(row=5, column=0)
+grey5.grid(row=6, column=0)
 grey6 = Button(frame2, text="Grey6", font=("Calibri, 10"), bg='grey11', fg='white', width=13, height=2, command=lambda m=6:change_colour(m))
-grey6.grid(row=6, column=0)
+grey6.grid(row=7, column=0)
 black = Button(frame2, text="Black", font=("Calibri, 10"), bg='grey1', fg='white', width=13, height=2, command=lambda m=7:change_colour(m))
-black.grid(row=7, column=0)
+black.grid(row=8, column=0)
 
 #colour button
 allwhite = Button(frame3, text="All White",font=("Calibri, 12"), bg='white', width=13, height=2, command=allwht)
